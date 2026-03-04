501b86cb2441b1d18fd84ccf5eb3bb5c4e385909
Neptune02-5 <mamounekatni@gmail.com>
2026-03-04 22:13:30 +0100

    organisation du dépot

--- tests/test_agent_client.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from agent_stock_mcp import run_cli
-
-@pytest.mark.asyncio
-async def test_cli_runs(monkeypatch):
-    monkeypatch.setattr("builtins.input", lambda _: "Hello")
-    await run_cli()
